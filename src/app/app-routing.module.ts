a16cb05ac9cfdc8be8377cf21bcdf2f970bbfe31
Rodrigo Fuentes <rdgfuentes+github@gmail.com>
2018-11-20 20:25:58 -0300

    Routes for new list view and dashboard view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CoinListComponent } from './coin-list/coin-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
-  }
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'list',
+    component: CoinListComponent,
+  },
 ];
 
 @NgModule({
